60fce772bf52a67241d343e4ab227147d55256e3
Jon Morton <jonanin@gmail.com>
2015-08-17 10:32:05 -0500

    Use rsync -a instead of rsync -rp

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cd ${0:a:h}
 git pull origin master
 git submodule update --init --recursive
 
-rsync --exclude ".git/" --exclude ".DS_Store" --exclude "setup.sh" --exclude "README.md" --exclude ".gitmodules" -rp . ~;
+rsync --exclude ".git/" --exclude ".DS_Store" --exclude "setup.sh" --exclude "README.md" --exclude ".gitmodules" -a . ~;
 (cd ~/.emacs.d ; cask install)
 
 if [ -z $ZSH_NAME ]; then

b7d30494fb669a24a953e549a87b548a6d4130c1
Jon Morton <jonanin@gmail.com>
2015-08-18 04:29:33 -0500

    emacs: Add rectangle insert shortcut

--- .emacs.d/customizations/shortcuts.el
+++ .emacs.d/customizations/shortcuts.el
@@ -1,5 +1,6 @@
 
 (smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
 
+(global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x r a") 'string-insert-rectangle)

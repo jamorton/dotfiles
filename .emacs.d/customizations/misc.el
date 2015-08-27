ffbe55bfb30f16f498df9af6c9dce62d291d3fb1
Jon Morton <jonanin@gmail.com>
2015-08-27 13:30:20 -0500

    Add some comments to misc.el

--- .emacs.d/customizations/misc.el
+++ .emacs.d/customizations/misc.el
@@ -1,12 +1,14 @@
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+;; Store backup files in cenrtalized directory
 (setq backup-directory-alist
   `(("." . ,(expand-file-name
              (concat
               (file-name-directory load-file-name)
               "backups")))))
 
+;; Load PATH from the shell so exec works
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 

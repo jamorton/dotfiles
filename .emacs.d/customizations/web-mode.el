5c02eafed27ea4701d2b49e81d6ec8caf6f42408
Jon Morton <jonanin@gmail.com>
2015-08-27 13:29:58 -0500

    Add web-mode for editing JSX

---
+++ .emacs.d/customizations/web-mode.el
@@ -0,0 +1,12 @@
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+;; adjust indents for web-mode to 2 spaces
+(defun my-web-mode-hook ()
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+(flycheck-add-mode 'javascript-eslint 'web-mode)

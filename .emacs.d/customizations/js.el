db940412439b946716d77bc77650f60fd1ff2df7
Jon Morton <jonanin@gmail.com>
2015-08-27 16:48:08 -0500

    make js2-mode not complain about trailing commas

--- .emacs.d/customizations/js.el
+++ .emacs.d/customizations/js.el
@@ -3,3 +3,7 @@
 
 (setq js-indent-level 2)
 (setq js2-basic-offset 2)
+
+;; Let flycheck handle parse errors
+(setq-default js2-show-parse-errors nil)
+(setq-default js2-strict-trailing-comma-warning nil)
